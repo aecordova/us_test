d8cb8221b94e081e68db8387fa3af6f37220f854
aecordova <ae_cordova@outlook.com>
2021-04-12 02:31:08 -0500

    Modify controllers for destroy show and index actions

--- app/controllers/api/v1/real_states_controller.rb
+++ app/controllers/api/v1/real_states_controller.rb
@@ -1,4 +1,12 @@
 class Api::V1::RealStatesController < ApplicationController
+  def index
+    all_real_states
+  end
+
+  def show
+    render json: { data: real_state }, status: :ok
+  end
+
   def create
     real_state = RealState.new(real_state_params)
 
@@ -9,6 +17,14 @@ class Api::V1::RealStatesController < ApplicationController
     end
   end
 
+  def destroy
+    if real_state.destroy
+      head :ok
+    else
+      head :unprocessable_entity
+    end
+  end
+
   private
 
   def real_state_params
@@ -16,5 +32,13 @@ class Api::V1::RealStatesController < ApplicationController
       :name, :property_type, :street, :external_number, :internal_number,
       :neighborhood, :city, :country, :rooms, :bathrooms, :comments
     )
+  end
+
+  def real_state
+    @real_state = RealState.find(params[:id])
+  end
+
+  def all_real_states
+    @all_real_states = RealState.all
   end
 end
---
+++ app/views/api/v1/real_states/index.json.jbuilder
@@ -0,0 +1,3 @@
+json.data do
+  json.array! @all_real_states, :name, :property_type, :city, :country
+end
