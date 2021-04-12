14a17dab4dd47a708d3884f6c1e2bf6e17fe1916
aecordova <ae_cordova@outlook.com>
2021-04-12 02:29:48 -0500

    Add specs for show, destroy and index actions

--- spec/requests/api/v1/real_states_request_spec.rb
+++ spec/requests/api/v1/real_states_request_spec.rb
@@ -48,16 +48,65 @@ RSpec.describe 'Api::V1::RealStates', type: :request do
       end
 
       it 'contains the list of errors in the body' do
-        expected_errors = { 
+        expected_errors = {
           name: ["can't be blank"],
           external_number: ['only allows alfanumeric and dashes'],
           internal_number: ['only allows alfanumeric, dashes and spaces'],
           country: ['is not included in the list'],
           rooms: ['is not a number'],
-          bathrooms: ['is not a number'] 
+          bathrooms: ['is not a number']
         }
         expect(json_response['errors']).to include_json(expected_errors)
       end
+    end
+  end
+
+  describe 'real_state #show' do
+    let(:property) { create(:real_state) }
+
+    context 'with a valid id in params' do
+      before { get "/api/v1/real_states/#{property.id}" }
+
+      it 'returns the requested record information' do
+        expected_attributes = attributes_for(:real_state)
+        expect(json_response['data']).to include_json(expected_attributes)
+      end
+    end
+
+    context ' with an invalid_id in params' do
+      before { get '/api/v1/real_states/meep' }
+      it 'responds with status: not_found' do
+        expect(response).to have_http_status(:not_found)
+      end
+    end
+  end
+
+  describe 'real_state #index' do
+    before do
+      create(:real_state, name: 'property 1')
+      create(:real_state, name: 'property 2')
+
+      get '/api/v1/real_states', headers: { 'ACCEPT' => 'application/json' }
+    end
+
+    it 'returns a list of real states with certain attributes' do
+      expected_object = { 'name' => 'property 1',
+                          'property_type' => 'house',
+                          'city' => 'somecity',
+                          'country' => 'MX' }
+
+      expect(json_response['data'].first).to match(expected_object)
+      expect(json_response['data'].size).to eql(2)
+    end
+  end
+
+  describe 'real_state #destroy' do
+    let(:created_record_id) { create(:real_state).id }
+
+    before { delete "/api/v1/real_states/#{created_record_id}", headers: { 'ACCEPT' => 'application/json' } }
+
+    it 'deletes the record' do
+      expect(RealState.find_by(id: created_record_id)).to be nil
     end
   end
 
